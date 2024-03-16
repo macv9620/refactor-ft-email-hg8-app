3ec53f6e3155ea6ca8d33586b94bb35cc2404a84
Mateo Vasco <macv9620@utp.edu.co>
2024-03-16 11:31:56 -0500

    Refactor home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,21 @@
 import { useState, useEffect } from "react";
-import ListEmail from "../components/Home/ListEmail";
-import ViewEmail from "../components/Home/ViewEmail";
-import { Avatar } from "@nextui-org/react";
-import { Button } from "@nextui-org/react";
-import { useStateProvider } from "../context/StateContext";
-import { reducerCase } from "../context/constants";
-import EmailType from "../types/EmailType";
-import ModalEmail from "../components/NewEmail/ModalEmail";
-import emailService from "../services/emailService";
+import { Avatar, Button } from "@nextui-org/react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router";
+import ListEmail from "../components/Home/ListEmail";
+import ViewEmail from "../components/Home/ViewEmail";
+import ModalEmail from "../components/NewEmail/ModalEmail";
+import { useStateProvider } from "../context/StateContext";
+import { reducerCase } from "../context/constants";
+import emailService from "../services/emailService";
+import EmailType from "../types/EmailType";
 
 const Home = () => {
-  const [{}, dispatch] = useStateProvider();
-
+  const [, dispatch] = useStateProvider();
   const [emails, setEmails] = useState<EmailType[]>([]);
   const [emailSelected, setEmailSelected] = useState<EmailType | null>(null);
   const [userName, setUserName] = useState<string>("");
   const [userMail, setUserMail] = useState<string>("");
-
   const navigate = useNavigate();
 
   const handleEmailSelected = (id: string) => {
@@ -27,51 +24,45 @@ const Home = () => {
   };
 
   useEffect(() => {
-    emailService
-      .getEmails()
-      .then((res) => {
-        console.log(res);
+    const fetchEmails = async () => {
+      try {
+        const res = await emailService.getEmails();
         const sortedEmails = res.data.sort(
-          (a: { timestamp: string }, b: { timestamp: string }) => {
-            return (
-              new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
-            );
-          }
+          (a: { timestamp: string }, b: { timestamp: string }) =>
+            new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
         );
         setEmails(sortedEmails);
-      })
-      .catch(() => {
+      } catch (error) {
         toast("No has iniciado sesion", { type: "error" });
         navigate("/login");
-      });
+      }
+    };
 
-    const userNameFromLocal = localStorage
-      .getItem("userName")
-      ?.toString()
-      .replace('"', "")
-      .replace('"', "");
-    const userMailFromLocal = localStorage
-      .getItem("email")
-      ?.toString()
-      .replace('"', "")
-      .replace('"', "");
+    const getUserInfoFromLocalStorage = () => {
+      const userNameFromLocal = localStorage
+        .getItem("userName")
+        ?.toString()
+        .replace('"', "")
+        .replace('"', "");
+      const userMailFromLocal = localStorage
+        .getItem("email")
+        ?.replace('"', "")
+        .replace('"', "");
+      if (userNameFromLocal && userMailFromLocal) {
+        setUserName(userNameFromLocal);
+        setUserMail(userMailFromLocal);
+      }
+    };
 
-    if (userNameFromLocal && userMailFromLocal) {
-      setUserName(userNameFromLocal);
-      setUserMail(userMailFromLocal);
-      console.log("User name from local");
-      console.log(userNameFromLocal);
-    }
-  }, []);
+    fetchEmails();
+    getUserInfoFromLocalStorage();
+  }, [navigate]);
 
   const logout = () => {
     localStorage.removeItem("access_token");
     localStorage.removeItem("email");
     toast("Sesión cerrada", { type: "success" });
-    dispatch({
-      type: reducerCase.SET_USER_INFO,
-      userInfo: null,
-    });
+    dispatch({ type: reducerCase.SET_USER_INFO, userInfo: null });
     navigate("/login");
   };
 
@@ -81,7 +72,6 @@ const Home = () => {
         <div className="text-md text-blue-500 font-bold">
           <p className="text-center pl-9">App Messages</p>
         </div>
-
         <div className="text-md font-bold">
           <p className="text-center pl-9">Welcome: {userName}</p>
         </div>
@@ -90,10 +80,7 @@ const Home = () => {
         </div>
         <div className="flex flex-row gap-3 pl-2 h-[30px]">
           <Avatar
-            src={
-              "https://placehold.co/155x232/black/white?text=" +
-              userName[0]?.toLocaleUpperCase()
-            }
+            src={`https://placehold.co/155x232/black/white?text=${userName[0]?.toLocaleUpperCase()}`}
           />
           <Button color="primary" variant="bordered" onClick={logout}>
             Cerrar Sesión
