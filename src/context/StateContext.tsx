01ff4a1ee402ffbd30cfa41ff7ee40dd5902f914
Mateo Vasco <macv9620@utp.edu.co>
2024-03-17 11:21:09 -0500

    Refactor StateContext to use generics

--- src/context/StateContext.tsx
+++ src/context/StateContext.tsx
@@ -1,11 +1,29 @@
-import { createContext, useReducer, useContext } from "react";
+import { createContext, useReducer, useContext, ReactNode, Reducer, Dispatch } from "react";
 
-export const StateContext = createContext({} as any);
+type ReducerType<S, A> = Reducer<S, A>;
+type InitialStateType<S> = S;
+type ChildrenType = ReactNode;
 
-export const StateProvider = ({ initialState, reducer, children }: any) => (
-  <StateContext.Provider value={useReducer(reducer, initialState)}>
-    {children}
-  </StateContext.Provider>
-);
+type StateContextType<S, A> = {
+  state: S;
+  dispatch: Dispatch<A>;
+};
 
-export const useStateProvider = () => useContext(StateContext);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const StateContext = createContext({} as StateContextType<any, any>);
+
+export const StateProvider = <S, A>({ initialState, reducer, children }: {
+  initialState: InitialStateType<S>;
+  reducer: ReducerType<InitialStateType<S>, A>;
+  children: ChildrenType;
+}) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <StateContext.Provider value={{ state, dispatch }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useStateProvider = <S, A>() => useContext(StateContext) as StateContextType<S, A>;
